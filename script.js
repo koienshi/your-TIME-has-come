e64d55635a50e5b6a4e4b5f4a7a0078ea3c9c6b2
anastasia21112 <anad221122@gmail.com>
2020-08-28 19:38:56 -0600

    clocks

--- script.js
+++ script.js
@@ -33,95 +33,115 @@ const clocks = document.querySelectorAll('.clock');
 
 // get rid of for loop and make it a function 
 
-// for (var i=0; i<clocks.length; i++) {
-//   var currentClock = clocks[i];
-//   var gmtOffset = currentClock.dataset.timezone;
-//   console.log(gmtOffset);
-//   const hr = currentClock.querySelector('.hr');
-//   const mn = currentClock.querySelector('.mn');
-//   const sc = currentClock.querySelector('.sc');
-//   var time = document.querySelector('.time');
-
- // document.querySelector('h1').innerHTML = currentClock.dataset.title;
-  
-  // setInterval(() => {
-  //   let day = new Date();
-
-  //   let s = day.getUTCSeconds();
-  //   let ss = s * 6;
-  //   let ms = day.getUTCMilliseconds();
-  //   let mss = (ms * 0.006) + +ss;
-    
-  //   let m = day.getUTCMinutes();
-  //   let mm = m * 6; //60min * 6 = 360 (degree around circle)
-
-  //   let h = +day.getUTCHours() + +gmtOffset; //prepended w/ "+" to parse into number
-  //   if (h < 0) h = 24 + +h;
-  //   let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
-    
-  //   hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
-  //   mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
-  //   sc.style.transform = `rotateZ(${mss}deg)`;
-
-  //   time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); //adds beginning 0 if under 10
-  // })
-//}
-
-// GMT TIME HTTP REQUEST
-var gmt_time;
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if(this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(xhttp.responseText);
-      gmt_time = data.datetime;
-      console.log(gmt_time);
-      createClock(gmt_time);
-    
-
-  }
-
-  if(xhttp.status == 404) {
-    console.log('File not found');
-  }
-  
-}
-xhttp.open("GET", "http://worldtimeapi.org/api/timezone/Etc/GMT", false);
-xhttp.send();
-
-
-function createClock(response) {
-  var currentClock = clocks[0];
+for (var i=0; i<clocks.length; i++) {
+  var currentClock = clocks[i];
+  var gmtOffset = currentClock.dataset.timezone;
+  console.log(gmtOffset);
   const hr = currentClock.querySelector('.hr');
   const mn = currentClock.querySelector('.mn');
   const sc = currentClock.querySelector('.sc');
   var time = document.querySelector('.time');
 
+ //document.querySelector('h1').innerHTML = currentClock.dataset.title;
   
-  document.querySelector('h1').innerHTML = currentClock.dataset.title;
-  var hour = response.slice(11, 13);
-  var minute = response.slice(14, 16);
-  var second = response.slice(17, 19);
-  var millisecond = response.slice(20, 26);
+  setInterval(() => {
+    let day = new Date();
 
-  let s = second;
-  let ss = s * 6;
-  let ms = millisecond;
-  let mss = (ms * 0.006) + +ss;
+    let s = day.getUTCSeconds();
+    let ss = s * 6;
+    let ms = day.getUTCMilliseconds();
+    let mss = (ms * 0.006) + +ss;
     
-  let m = minute;
-  let mm = m * 6; //60min * 6 = 360 (degree around circle)
+    let m = day.getUTCMinutes();
+    let mm = m * 6; //60min * 6 = 360 (degree around circle)
 
-  let h = hour; //prepended w/ "+" to parse into number
-  if (h < 0) h = 24 + +h;
-  let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
+    if (i == 0) {
+      let h = +day.getUTCHours() + +gmtOffset;
+    } else if (i == 1) {
+      let h = +day.getUTCHours + +gmtOffset;
+      let rh = h - 4;
+    } else if (i == 2) {
+      let h = +day.getUTCHours() + +gmtOffset;
+      h -= 6;
+    } else if (i == 3) {
+      let h = +day.getUTCHours() + +gmtOffset;
+      h -= 7;
+    } else if (i == 4) {
+      let h = +day.getUTCHours() + +gmtOffset;
+      h -= 9;
+    } else if (i == 5) {
+      let h = +day.getUTCHours() + +gmtOffset;
+      h += 1;
+    }
+     //prepended w/ "+" to parse into number
+    if (h < 0) h = 24 + +h;
+    let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
     
-  hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
-  mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
-  sc.style.transform = `rotateZ(${mss}deg)`;
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
+    mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
+    sc.style.transform = `rotateZ(${mss}deg)`;
 
-  time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); 
-  
+    time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); //adds beginning 0 if under 10
+  })
 }
+
+// GMT TIME HTTP REQUEST
+// var gmt_time;
+// const xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function() {
+//   if(this.readyState == 4 && this.status == 200) {
+//       var data = JSON.parse(xhttp.responseText);
+//       gmt_time = data.datetime;
+//       console.log(gmt_time);
+//       createClock(gmt_time);
+    
+
+//   }
+
+//   if(xhttp.status == 404) {
+//     console.log('File not found');
+//   }
+  
+// }
+// xhttp.open("GET", "http://worldtimeapi.org/api/timezone/Etc/GMT", true);
+// xhttp.send();
+
+
+// function createClock(response) {
+//   var currentClock = clocks[0];
+//   const hr = currentClock.querySelector('.hr');
+//   const mn = currentClock.querySelector('.mn');
+//   const sc = currentClock.querySelector('.sc');
+//   var time = document.querySelector('.time');
+
+  
+  
+//   setInterval (() => {
+   
+//   var hour = response.slice(11, 13);
+//   var minute = response.slice(14, 16);
+//   var second = response.slice(17, 19);
+//   var millisecond = response.slice(20, 26);
+
+//   let s = second;
+//   let ss = s * 6;
+//   let ms = millisecond;
+//   let mss = (ms * 0.006) + +ss;
+    
+//   let m = minute;
+//   let mm = m * 6; //60min * 6 = 360 (degree around circle)
+
+//   let h = hour; //prepended w/ "+" to parse into number
+//   if (h < 0) h = 24 + +h;
+//   let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
+    
+//   hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
+//   mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
+//   sc.style.transform = `rotateZ(${mss}deg)`;
+
+//   time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); 
+//   })
+// }
 
 // one function that takes in the response text and chops it up
 // in the function, want four things – hours, minutes, seconds, milliseconds
