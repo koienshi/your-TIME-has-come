a0accdfc2658dd12ff8191d5d8387d8e489ed276
anastasia21112 <anad221122@gmail.com>
2020-08-28 19:17:36 -0600

    clocks

--- script.js
+++ script.js
@@ -31,48 +31,53 @@ function showSlides(n) {
 // CLOCK ARMS
 const clocks = document.querySelectorAll('.clock');
 
-for (var i=0; i<clocks.length; i++) {
-  var currentClock = clocks[i];
-  var gmtOffset = currentClock.dataset.timezone;
-  console.log(gmtOffset);
-  const hr = currentClock.querySelector('.hr');
-  const mn = currentClock.querySelector('.mn');
-  const sc = currentClock.querySelector('.sc');
-  var time = document.querySelector('.time');
+// get rid of for loop and make it a function 
 
-  //document.querySelector('h1').innerHTML = currentClock.dataset.title;
+// for (var i=0; i<clocks.length; i++) {
+//   var currentClock = clocks[i];
+//   var gmtOffset = currentClock.dataset.timezone;
+//   console.log(gmtOffset);
+//   const hr = currentClock.querySelector('.hr');
+//   const mn = currentClock.querySelector('.mn');
+//   const sc = currentClock.querySelector('.sc');
+//   var time = document.querySelector('.time');
+
+ // document.querySelector('h1').innerHTML = currentClock.dataset.title;
   
-  setInterval(() => {
-    let day = new Date();
+  // setInterval(() => {
+  //   let day = new Date();
 
-    let s = day.getUTCSeconds();
-    let ss = s * 6;
-    let ms = day.getUTCMilliseconds();
-    let mss = (ms * 0.006) + +ss;
+  //   let s = day.getUTCSeconds();
+  //   let ss = s * 6;
+  //   let ms = day.getUTCMilliseconds();
+  //   let mss = (ms * 0.006) + +ss;
     
-    let m = day.getUTCMinutes();
-    let mm = m * 6; //60min * 6 = 360 (degree around circle)
+  //   let m = day.getUTCMinutes();
+  //   let mm = m * 6; //60min * 6 = 360 (degree around circle)
 
-    let h = +day.getUTCHours() + +gmtOffset; //prepended w/ "+" to parse into number
-    if (h < 0) h = 24 + +h;
-    let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
+  //   let h = +day.getUTCHours() + +gmtOffset; //prepended w/ "+" to parse into number
+  //   if (h < 0) h = 24 + +h;
+  //   let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
     
-    hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
-    mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
-    sc.style.transform = `rotateZ(${mss}deg)`;
+  //   hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
+  //   mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
+  //   sc.style.transform = `rotateZ(${mss}deg)`;
 
-    time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); //adds beginning 0 if under 10
-  })
-}
+  //   time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); //adds beginning 0 if under 10
+  // })
+//}
 
+// GMT TIME HTTP REQUEST
+var gmt_time;
 const xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
-  if(xhttp.readyState == 4) {
-    if(xhttp.status == 200) {
-      console.log(xhttp.responseText);
-      gmt_time = xhttp.responseTime.getElementById['datetime'];
+  if(this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(xhttp.responseText);
+      gmt_time = data.datetime;
       console.log(gmt_time);
-    }
+      createClock(gmt_time);
+    
+
   }
 
   if(xhttp.status == 404) {
@@ -80,11 +85,108 @@ xhttp.onreadystatechange = function() {
   }
   
 }
-
-const API_URL = "http://worldtimeapi.org/api/timezone"; 
-
-xhttp.open("GET", "http://worldtimeapi.org/api/timezone/Etc/GMT", true);
+xhttp.open("GET", "http://worldtimeapi.org/api/timezone/Etc/GMT", false);
 xhttp.send();
+
+
+function createClock(response) {
+  var currentClock = clocks[0];
+  const hr = currentClock.querySelector('.hr');
+  const mn = currentClock.querySelector('.mn');
+  const sc = currentClock.querySelector('.sc');
+  var time = document.querySelector('.time');
+
+  
+  document.querySelector('h1').innerHTML = currentClock.dataset.title;
+  var hour = response.slice(11, 13);
+  var minute = response.slice(14, 16);
+  var second = response.slice(17, 19);
+  var millisecond = response.slice(20, 26);
+
+  let s = second;
+  let ss = s * 6;
+  let ms = millisecond;
+  let mss = (ms * 0.006) + +ss;
+    
+  let m = minute;
+  let mm = m * 6; //60min * 6 = 360 (degree around circle)
+
+  let h = hour; //prepended w/ "+" to parse into number
+  if (h < 0) h = 24 + +h;
+  let hh = h * 30; //hours hand loops around twice (720) (AM + PM)
+    
+  hr.style.transform = `rotateZ(${(hh)+(mm/12)+(mss/3600)}deg)`;
+  mn.style.transform = `rotateZ(${(mm)+(mss/60)}deg)`;
+  sc.style.transform = `rotateZ(${mss}deg)`;
+
+  time.innerHTML = ('0' + h).slice(-2) + ':' + ('0' + m).slice(-2) + ':' + ('0' + s).slice(-2); 
+  
+}
+
+// one function that takes in the response text and chops it up
+// in the function, want four things – hours, minutes, seconds, milliseconds
+// assign each part of the chopped up part to a specific var
+// call a bigger func and have those variables to be the args int he new func
+// use those times to use the setinterval function for the clock
+
+//NY TIME HTTP REQUEST
+// const xhttp1 = new XMLHttpRequest();
+// xhttp1.onreadystatechange = function() {
+//   if(this.readyState == 4 && this.status == 200) {
+//       var data = JSON.parse(xhttp1.responseText);
+//       var ny_time = data.datetime;
+//       console.log(ny_time);
+//   }
+
+//   if(xhttp1.status == 404) {
+//     console.log('File not found');
+//   }
+  
+// }
+// xhttp1.open("GET", "http://worldtimeapi.org/api/timezone/America/New_York.txt", true);
+// xhttp1.send();
+
+
+// //MOUNTAIN TIME HTTP REQUEST
+
+// const xhttp2 = new XMLHttpRequest();
+// xhttp2.onreadystatechange = function() {
+//   if(this.readyState == 4 && this.status == 200) {
+//       var data = JSON.parse(xhttp2.responseText);
+//       var mdt_time = data.datetime;
+//       console.log(mdt_time);
+//   }
+
+//   if(xhttp2.status == 404) {
+//     console.log('File not found');
+//   }
+  
+// }
+// xhttp2.open("GET", "http://worldtimeapi.org/api/timezone/America/Denver.txt", true);
+// xhttp2.send();
+
+
+// //PACIFIC TIME HTTP REQUEST
+// const xhttp3 = new XMLHttpRequest();
+// xhttp3.onreadystatechange = function() {
+//   if(this.readyState == 4 && this.status == 200) {
+//       var data = JSON.parse(xhttp3.responseText);
+//       var ny_time = data.datetime;
+//       console.log(ny_time);
+//   }
+
+//   if(xhttp2.status == 404) {
+//     console.log('File not found');
+//   }
+  
+// }
+// xhttp2.open("GET", "http://worldtimeapi.org/api/timezone/America/Denver.txt", true);
+// xhttp2.send();
+
+
+
+
+
 // xhttp.open("GET", "http://worldtimeapi.org/api/timezone/America/New_York.txt");
 // xhttp.send();
 // console.log('here1');
